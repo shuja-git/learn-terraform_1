5eeb0985564221a794532c49c78391591f9776ff
shuja <shuja.gmail.com>
2023-04-15 17:27:15 +0530

    DNS record creation

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -6,6 +6,13 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "frontend-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 resource "aws_instance" "cart" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
@@ -14,6 +21,14 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "cart-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
@@ -21,6 +36,13 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "catalogue-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
 resource "aws_instance" "shipping" {
   ami = "ami-0089b8e98cd95257d"
@@ -30,6 +52,13 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "shipping-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 resource "aws_instance" "user" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
@@ -37,6 +66,13 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+resource "aws_route53_record" "user" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "user-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 resource "aws_instance" "payment" {
   ami = "ami-0089b8e98cd95257d"
@@ -46,6 +82,13 @@ resource "aws_instance" "payment" {
     Name = "payment"
   }
 }
+resource "aws_route53_record" "payment" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "payment-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
 resource "aws_instance" "mongodb" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
@@ -53,6 +96,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "mongodb-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 resource "aws_instance" "mysql" {
   ami = "ami-0089b8e98cd95257d"
@@ -62,6 +112,13 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "mysql-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 resource "aws_instance" "rabbitmq" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
@@ -70,6 +127,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "rabbitmq-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 resource "aws_instance" "redis" {
   ami = "ami-0089b8e98cd95257d"
   instance_type = "t2.micro"
@@ -77,4 +141,11 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z10218511FGAD8YC6L1HI"
+  name    = "redis-dev.shujadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
