69bf08b4fc4072c1124da72935b4e60ceee6d6f3
shuja <shuja.gmail.com>
2023-04-15 14:24:33 +0530

    testing instance creation

---
+++ 01-sample/test.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0089b8e98cd95257d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0f814c32290173c7c"]
+  tags = {
+    Name = "test"
+  }
+}
