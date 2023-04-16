5cf09babdfd1a22d2891c6e0b01355e63618626b
shuja <shuja.gmail.com>
2023-04-16 17:42:15 +0530

    immature code example

--- roboshop-setup/ex3/main.tf
+++ roboshop-setup/ex3/main.tf
@@ -4,26 +4,46 @@ data "aws_ami" "ami" {
   owners           = ["973714476881"]
 }
 
+#resource "aws_instance" "frontend" {
+#  for_each = var.names
+#  ami =  data.aws_ami.ami.image_id
+#  instance_type = each.value["type"]
+#  vpc_security_group_ids = ["sg-0f814c32290173c7c"]
+#
+#  tags = {
+#    Name = each.value["name"]
+#  }
+#}
+
 resource "aws_instance" "frontend" {
-  for_each = var.instances
+  count = length(var.names)
   ami =  data.aws_ami.ami.image_id
-  instance_type = each.value["type"]
+  instance_type =  var.types[count.index]
   vpc_security_group_ids = ["sg-0f814c32290173c7c"]
 
   tags = {
-    Name = each.value["name"]
+    Name = var.names[count.index]
   }
 }
 
-variable "instances" {
-  default = {
-    catalogue = {
-      name = "catalogue"
-      type = "t2.micro"
-    }
-    user = {
-      name = "user"
-      type = "t3.micro"
-    }
-  }
+
+#Immature code
+variable "names" {
+  default = ["catalogue","cart"]
 }
+variable "types" {
+  default = ["t2.micro", "t3.micro"]
+}
+
+#variable "instances" {
+#  default = {
+#    catalogue = {
+#      name = "catalogue"
+#      type = "t2.micro"
+#    }
+#    user = {
+#      name = "user"
+#      type = "t3.micro"
+#    }
+#  }
+#}
