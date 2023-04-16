8881d660fdd6626a7e69e9738675537c0a727ac2
shuja <shuja.gmail.com>
2023-04-16 15:24:20 +0530

    count with static number testing

---
+++ roboshop-setup/ex2/main.tf
@@ -0,0 +1,13 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  count = 5
+  ami =  data.aws_ami.ami.image_id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0f814c32290173c7c"]
+
+}
