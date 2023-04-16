cad419d06641dda43efce935f50a4ef1edc8ae62
shuja <shuja.gmail.com>
2023-04-16 21:44:23 +0530

    create instance or not

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -3,3 +3,11 @@ resource "aws_instance" "ec2" {
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
 }
 variable "instance_type" {}
+
+resource "aws_instance" "ec2" {
+  count = var.create_instance == "true" ? 1 : 0
+  ami = "ami-0089b8e98cd95257d"
+  instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
+}
+variable "create_instance" {}
+
