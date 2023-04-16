37e1bcace71956a5ab73f7f4511e12c45a9a045c
shuja <shuja.gmail.com>
2023-04-16 21:50:20 +0530

    string to boolean function

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ec2" {
 variable "instance_type" {}
 
 resource "aws_instance" "ec21" {
-  count = var.create_instance == "true" ? 1 : 0
+  count = tobool(var.create_instance) ? 1 : 0
   ami = "ami-0089b8e98cd95257d"
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
 }
