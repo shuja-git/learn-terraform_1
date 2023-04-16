0fab0dde040e9b277fc77713ba04c8329e7e1cf0
shuja <shuja.gmail.com>
2023-04-16 15:39:11 +0530

    testing list of map values

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -1,45 +1,61 @@
-variable "sample" {
-  default = "Hello World"
-}
-output "sample" {
-  value = var.sample
-}
-variable "sample1" {}
-output "sample1" {
-  value = var.sample1
-}
-variable "cli" {}
-output "cli" {
-  value = var.cli
-}
-variable "env" {}
-output "environment_variable" {
-  value = var.env
-}
-variable "sample2" {
-  default = "sample2 value"
-}
-variable "sample3_list" {
+#variable "sample" {
+#  default = "Hello World"
+#}
+#output "sample" {
+#  value = var.sample
+#}
+#variable "sample1" {}
+#output "sample1" {
+#  value = var.sample1
+#}
+#variable "cli" {}
+#output "cli" {
+#  value = var.cli
+#}
+#variable "env" {}
+#output "environment_variable" {
+#  value = var.env
+#}
+#variable "sample2" {
+#  default = "sample2 value"
+#}
+#variable "sample3_list" {
+#  default = [
+#    "Hello",
+#    "World",
+#    100,
+#    true
+#  ]
+#}
+#variable "sample_map" {
+#  default = {
+#    string = "Helloooo",
+#    number = 100,
+#    boolean = true
+#  }
+#}
+#output "sample2" {
+#  value = "The single value is ${var.sample2}"
+#}
+#output "printing_list" {
+#  value = "The first value of list is ${var.sample3_list[0]}"
+#}
+#output "printing_map" {
+#  value = "The boolean value is ${var.sample_map["boolean"]}"
+#}
+
+variable "d1" {
   default = [
-    "Hello",
-    "World",
-    100,
-    true
+    {
+      course_name = "devops"
+      trainer_name = "shuja"
+    },
+    {
+      course_name = "aws"
+      trainer_name = "Mohd"
+    }
   ]
 }
-variable "sample_map" {
-  default = {
-    string = "Helloooo",
-    number = 100,
-    boolean = true
-  }
-}
-output "sample2" {
-  value = "The single value is ${var.sample2}"
-}
-output "printing_list" {
-  value = "The first value of list is ${var.sample3_list[0]}"
-}
-output "printing_map" {
-  value = "The boolean value is ${var.sample_map["boolean"]}"
+output "course" {
+  value = var.d1.*.course_name
 }
