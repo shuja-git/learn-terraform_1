2002cd53d535a60ede46df16d262f826075fa98a
shuja <shuja.gmail.com>
2023-04-16 21:45:15 +0530

    name fixed

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2" {
 }
 variable "instance_type" {}
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec21" {
   count = var.create_instance == "true" ? 1 : 0
   ami = "ami-0089b8e98cd95257d"
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
